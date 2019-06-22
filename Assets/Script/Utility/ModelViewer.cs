91b612cb214b2a4cd6e92116d888ac857eff3db0
enchanted <enchanted.com>
2019-06-22 15:34:46 +0800

    add kinect model viewer

--- Assets/Script/Utility/ModelViewer.cs
+++ Assets/Script/Utility/ModelViewer.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ModelViewer : MonoBehaviour
 {
@@ -17,8 +18,12 @@ public class ModelViewer : MonoBehaviour
     public Quaternion originalRotation;
     public Quaternion relatedRotation;
 
+    private MyGestureListener gestureListener;
+    //public Text text;
+
     public void Rotate(Vector3 move)
     {
+        print("rotate");
         var euler = new Vector3(move.y * 360, -move.x * 360, -move.z * 360);
         transform.rotation = Quaternion.Euler(euler) * relatedRotation;
     }
@@ -69,11 +74,58 @@ public class ModelViewer : MonoBehaviour
         originalRotation = transform.rotation;
         //Rotate(new Vector2(0.0f, 0.2f));
         //Rotate(new Vector2(0.1f, 0f));
+
+        gestureListener = MyGestureListener.Instance;
+        print(gestureListener);
     }
 
     // Update is called once per frame
     void Update()
     {
         //Rotate(new Vector2(0.0f, 0.01f));
+
+        if (!gestureListener)
+        {
+            print("未成功加载 MyGestureListener");
+            return;
+        }
+
+        //if (gestureListener.IsSwipeUp())
+        //{
+        //    text.GetComponent<Text>().text = "向上挥动";
+        //    print("up");
+        //    if(upFlag == false)
+        //    {
+        //        upFlag = true;
+        //        Scale(0.1f);
+        //    }
+
+        //}
+        //if (gestureListener.IsSwipeDown())
+        //{
+        //    text.GetComponent<Text>().text = "向下挥动";
+        //    print("down");
+        //    if(upFlag == true)
+        //    {
+        //        upFlag = false;
+        //    }
+        //    else
+        //        Scale(-0.1f);
+        //}
+        if (gestureListener.IsRaiseRightHand())
+        {
+            //text.GetComponent<Text>().text = "右手举起过肩并保持至少一秒";
+            print("large");
+            BeginDrag();
+            Scale(0.2f);
+
+        }
+        if (gestureListener.IsRaiseLeftHand())
+        {
+            //text.GetComponent<Text>().text = "左手举起过肩并保持至少一秒";
+            print("small");
+            BeginDrag();
+            Scale(-0.2f);
+        }
     }
 }
