cbd7845c06354a70df50c4c2f71b2b11f90a3dff
Choconut <843940285@qq.com>
2019-06-24 14:23:03 +0800

    background1

---
+++ Assets/Script/KinectControl/BackgroundLoader.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackgroundLoader : MonoBehaviour
+{
+    public Camera foreground;
+    public Camera background;
+    public GUITexture image;
+    KinectManager manager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        manager = KinectManager.Instance;
+
+        if (manager)
+        {
+            OverlayController overlay = manager.GetComponent<OverlayController>();
+            overlay.foregroundCamera = foreground;
+            overlay.backgroundCamera = background;
+            overlay.backgroundImage = image;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (manager)
+        {
+            OverlayController overlay = manager.GetComponent<OverlayController>();
+            overlay.foregroundCamera = foreground;
+            overlay.backgroundCamera = background;
+            overlay.backgroundImage = image;
+        }
+
+    }
+}
